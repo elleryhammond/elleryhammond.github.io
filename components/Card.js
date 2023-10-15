663a24ccf7a1ec05e1f97a8dc6ef660b62ce8baa
Ellery Hammond <elleryclairehammond@gmail.com>
2023-10-14 21:55:11 -0700

    Card Class module

---
+++ components/Card.js
@@ -0,0 +1,43 @@
+export default class Card {
+  constructor({ name, link }, cardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+  }
+
+  _setEventListeners() {
+    //".card__like-button"
+    this._cardElement
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeButton();
+      });
+
+    //".card__delete-button"
+    this._cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteCardButton();
+      });
+  }
+
+  _handleLikeButton() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_active");
+  }
+
+  _handleDeleteCardButton() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+  getView() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    this._setEventListeners();
+  }
+}
