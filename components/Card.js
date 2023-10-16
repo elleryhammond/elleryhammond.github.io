9499a0ae0cd4f8825b1a5a2d11602fd3dffbb93a
Ellery Hammond <elleryclairehammond@gmail.com>
2023-10-16 12:05:44 -0700

    refactor

--- components/Card.js
+++ components/Card.js
@@ -1,3 +1,64 @@
+// export default class Card {
+//   constructor(cardData, cardSelector, handleImageClick) {
+//     this._name = cardData.name;
+//     this._link = cardData.link;
+//     this._cardSelector = cardSelector;
+//     this._handleImageClick = handleImageClick;
+//   }
+
+//   _getTemplate() {
+//     const cardElement = document
+//       .querySelector(this._cardSelector)
+//       .content.querySelector(".card")
+//       .cloneNode(true);
+
+//     return cardElement;
+//   }
+
+//   getView() {
+//     this._cardElement = this._getTemplate();
+//     this._setEventListeners();
+//     this._cardImage = this._cardElement.querySelector(".card__image");
+//     const cardTitle = this._cardElement.querySelector(".card__title");
+//     const cardImage = this._cardElement.querySelector(".card__image");
+//     cardTitle.textContent = this._name;
+//     cardImage.setAttribute("src", this._link);
+//     cardImage.setAttribute("alt", this._name);
+//     return this._cardElement;
+//   }
+
+//   _setEventListeners() {
+//     const cardLikeButton =
+//       this._cardElement.querySelector(".card__like-button");
+//     cardLikeButton.addEventListener("click", () => {
+//       this._handleLikeButton();
+//     });
+
+//     const cardDeleteButton = this._cardElement.querySelector(
+//       ".card__delete-button"
+//     );
+//     cardDeleteButton.addEventListener("click", () => {
+//       this._handleDeleteCardButton();
+//     });
+
+//     const cardImage = this._cardElement.querySelector(".card__image");
+//     cardImage.addEventListener("click", () => {
+//       this._handleImageClick();
+//     });
+//   }
+
+//   _handleLikeButton() {
+//     const cardLikeButton =
+//       this._cardElement.querySelector(".card__like-button");
+//     cardLikeButton.classList.toggle(".card__like-button_active");
+//   }
+
+//   _handleDeleteCardButton() {
+//     this._cardElement.remove();
+//     this._cardElement = null;
+//   }
+// }
+
 export default class Card {
   constructor(cardData, cardSelector, handleImageClick) {
     this._name = cardData.name;
