963f83e0d0ec88f06e31ee44af6c2d85d6bb8f40
Ellery Hammond <elleryclairehammond@gmail.com>
2023-10-31 07:38:45 -0700

    refactor

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,10 +6,12 @@ export default class Popup {
 
   open() {
     this._popupElement.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
     this._popupElement.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
   _handleEscClose = (event) => {
@@ -19,7 +21,6 @@ export default class Popup {
   };
 
   setEventListeners() {
-    document.addEventListener("keydown", this._handleEscClose);
     this._popupElement.addEventListener("click", (event) => {
       if (event.target.classList.contains("modal_opened")) {
         this.close();
