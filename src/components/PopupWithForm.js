37a18fe5e8785e197ab4f6b739707604b168e772
Ellery Hammond <elleryclairehammond@gmail.com>
2023-11-19 22:51:32 -0800

    refactor after second review

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -24,7 +24,6 @@ export default class PopupWithForm extends Popup {
     this._popupForm.addEventListener("submit", (event) => {
       event.preventDefault();
       this._handleFormSubmit(this._getInputValues());
-      this.close();
     });
   }
 
